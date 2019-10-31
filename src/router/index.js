8c001777942515d53a8a9a18002d21692dd011f2
Huominxing <1240619602@qq.com>
2019-10-31 11:29:45 +0800

    第二天首页基本模块实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import vue from 'vue'
 import VueRouter from 'vue-router'
-
 import Login from '@/views/login'
+import Home from '@/views/home'
+import welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 vue.use(VueRouter)
 
@@ -13,6 +15,23 @@ const router = new VueRouter({
     {
       path: '/login',
       component: Login
+    },
+    // 首页
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎界面
+        {
+          path: '/',
+          component: welcome
+        }
+      ]
+    },
+    // 404处理
+    {
+      path: '*',
+      component: NotFound
     }
   ]
 
